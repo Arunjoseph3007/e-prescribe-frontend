e484bb1257b3defd00c761f79df9caa072f8ada7
NiteshBhalala <niteshbhalala123@gmail.com>
2023-06-27 12:17:34 +0530

    patient/doctor added

--- src/pages/patient/doctor/[doctorId].tsx
+++ src/pages/patient/doctor/[doctorId].tsx
@@ -1,3 +1,87 @@
-export default function DoctorPage(){
-    return <main>doctor</main>
+import {
+    Heading,
+    Flex,
+    Box,
+    Text,
+    Link
+} from "@chakra-ui/react"
+
+import {
+    ChevronRightIcon
+} from "@chakra-ui/icons"
+// import moment from "moment";
+import Navbar from "@/components/Navbar"
+
+const sessions = [
+    {
+        id: 0,
+        title: "Cough and cold",
+        lastVisit: "23/03/23",
+        startDate: "a month ago",
+        noOfVisits: 4
+    },
+    {
+        id: 1,
+        title: "Fever",
+        lastVisit: "23/03/23",
+        startDate: "a month ago",
+        noOfVisits: 4
+    },
+    {
+        id: 2,
+        title: "Malaria",
+        lastVisit: "23/03/23",
+        startDate: "a month ago",
+        noOfVisits: 4
+    },
+    {
+        id: 3,
+        title: "Dengue",
+        lastVisit: "23/03/23",
+        startDate: "a month ago",
+        noOfVisits: 4
+    },
+
+]
+
+export default function DoctorPage() {
+    return (
+        <div>
+            <Navbar />
+            {sessions.map((session) => (
+                <Flex
+                    mx="auto"
+                    maxW="4xl"
+                    my={4}
+                    p={3}
+                    rounded="md"
+                    h="24"
+                    alignItems={"center"}
+                    shadow="md"
+                    borderWidth={1}
+                    borderColor="gray.100"
+                    key={session.id}
+                >
+                    <Box flex={1}>
+                        <Heading fontWeight={"medium"} size="md">
+                            {session.title}
+                        </Heading>
+                        <Text fontSize={"sm"} color="GrayText">
+                            Last visit {session.lastVisit}
+                        </Text>
+                        <Flex fontWeight="medium" gap={2}>
+                            <Text fontSize="sm">
+                                No Of Visits: {session.noOfVisits}
+                            </Text>
+                            <Text fontSize="sm">
+                                Started: {session.startDate}
+                            </Text>
+                        </Flex>
+                    </Box>
+                    <Link href={`/patient/session/${session.id}`}>
+                        <ChevronRightIcon fontSize={"4xl"} color={"GrayText"} />
+                    </Link>
+                </Flex>
+            ))}
+        </div>)
 }
--- src/pages/patient/session/[sessionId].tsx
+++ src/pages/patient/session/[sessionId].tsx
@@ -1,3 +1,213 @@
-export default function SessionPage(){
-    return <main>session</main>
+import Navbar from '@/components/Navbar'
+import {
+    Box,
+    Flex,
+    Tag,
+    Text,
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Button,
+    Heading,
+    Link,
+    Center,
+    Divider,
+    Collapse
+} from '@chakra-ui/react'
+import Dosage from '@/components/Dosage';
+import { useState } from 'react';
+
+const sessions = [
+    {
+        id: 0,
+        date: "20/06/23",
+        symptoms: ["Headache", "Fatigue", "Nausea", "Stomachache"]
+    },
+    {
+        id: 1,
+        date: "10/06/23",
+        symptoms: ["Breathing Problems", "Fever"]
+    },
+    {
+        id: 2,
+        date: "26/05/23",
+        symptoms: ["Indigestion", "Vomiting", "Nausea", "Gas"]
+    },
+    {
+        id: 3,
+        date: "13/04/23",
+        symptoms: ["Cough", "Fatigue", "Swelling"]
+    },
+]
+
+const presciptions = [
+    {
+        id: 0,
+        medName: "Paracetamol",
+        days: 5,
+        dosage: {
+            morning: 1,
+            afternoon: -1,
+            evening: 1
+        }
+    },
+    {
+        id: 1,
+        medName: "Ibuprofen",
+        days: 3,
+        dosage: {
+            morning: 1,
+            afternoon: 0,
+            evening: 1
+        }
+    },
+    {
+        id: 2,
+        medName: "Amlodipine",
+        days: 6,
+        dosage: {
+            morning: 1,
+            afternoon: 0,
+            evening: 1
+        }
+    },
+]
+
+
+export default function SessionPage() {
+    // const [expandedPanels, setExpandedPanels] = useState([]);
+    // const togglePanel = (panelIndex) => {
+    //     if (expandedPanels.includes(panelIndex)) {
+    //         setExpandedPanels(expandedPanels.filter((index) => index !== panelIndex));
+    //     } else {
+    //         setExpandedPanels([...expandedPanels, panelIndex]);
+    //     }
+    // };
+
+    return (
+        <div>
+            <Navbar />
+            <Box maxW="6xl" mx="auto">
+                {/* //` STATS */}
+                <Box h="56" my={4} rounded="md" bg="gray.200" display="flex" justifyContent="center" alignItems="center">
+                    <Heading>Dr Anil Shah</Heading>
+                </Box>
+                <Flex
+                    mx="auto"
+                    maxW="5xl"
+                    my={16}
+                    p={3}
+                    rounded="md"
+                    minH="200px"
+                    alignItems={"center"}
+                    shadow="md"
+                    borderWidth={1}
+                    borderColor="gray.100"
+                >
+                    <Box flex={1}>
+                        <Box>
+                            <Accordion allowMultiple>
+                                {sessions.map((val, index) => {
+                                    return (
+                                        <>
+                                            <AccordionItem border="none" key={index}>
+                                                <h2>
+                                                    <AccordionButton
+                                                        // onClick={() => togglePanel(index)}
+                                                    >
+                                                        <Box as="span" flex='1' textAlign='left'>
+                                                            {/* <Flex alignItems="center"> */}
+                                                            <Heading fontWeight={"medium"} size="md" mt={4}>
+                                                                <Text>Date: {val.date}</Text>
+                                                            </Heading>
+                                                            <Heading fontWeight={"medium"} size="md" my={4} >
+                                                                <Text>Symptoms:
+                                                                    {val.symptoms.map((sym, ind) => {
+                                                                        return (
+                                                                            <>
+                                                                                <Tag key={ind} size='md' mb={3} variant='solid' mx={2} colorScheme='teal'>{sym}</Tag>
+                                                                            </>
+                                                                        )
+                                                                    })}
+                                                                </Text>
+                                                            </Heading>
+                                                            {/* </Flex> */}
+
+                                                        </Box>
+                                                        <AccordionIcon />
+                                                    </AccordionButton>
+                                                </h2>
+                                                {/* <Collapse in={expandedPanels.includes(index)}></Collapse> */}
+                                                <AccordionPanel pb={4} >
+                                                    <Heading fontWeight={"medium"} size="md" mb="2">Prescription: </Heading>
+                                                    <Box rounded="md" border={"2px"} borderColor={"gray.100"}>
+                                                        <Flex
+                                                            borderBottom={"1px"}
+                                                            borderColor={"blackAlpha.200"}
+                                                            p={4}
+                                                            px={4}
+                                                            color={""}
+                                                            fontSize={"sm"}
+                                                        >
+                                                            <Heading size="sm" w="40%" >
+                                                                Medicine
+                                                            </Heading>
+                                                            <Heading w="30%" size="sm">
+                                                                <Center>Days</Center>
+                                                            </Heading>
+                                                            <Heading textAlign="center" w="30%" size="sm">
+                                                                Dosage
+                                                            </Heading>
+                                                        </Flex>
+
+                                                        {presciptions.map((presciption, i) => {
+                                                            return (
+                                                                <Flex
+                                                                    color="blackAlpha.600"
+                                                                    alignItems="center"
+                                                                    borderBottom={"1px"}
+                                                                    borderColor={"blackAlpha.200"}
+                                                                    fontWeight={"bold"}
+                                                                    key={i}
+                                                                    p={3}
+                                                                    px={4}
+                                                                    fontSize={"sm"}
+                                                                >
+                                                                    <Box
+                                                                        w="40%"
+                                                                    // pr={4}
+                                                                    // flex={1}
+                                                                    >
+                                                                        {presciption.medName}
+                                                                    </Box>
+                                                                    <Box w="30%">
+                                                                        <Center>{presciption.days}</Center>
+                                                                    </Box>
+                                                                    <Box w="30%">
+                                                                        <Center>
+                                                                            <Dosage
+                                                                                dosage={presciption.dosage}
+                                                                            />
+                                                                        </Center>
+                                                                    </Box>
+                                                                </Flex>
+                                                            )
+                                                        }
+                                                        )}
+                                                    </Box>
+                                                </AccordionPanel>
+                                            </AccordionItem>
+                                            <Divider />
+                                        </>
+                                    )
+                                })}
+                            </Accordion>
+                        </Box>
+                    </Box>
+                </Flex>
+            </Box>
+        </div>
+    )
 }
