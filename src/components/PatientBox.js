0b5fd148f8ac1c6bdc686ef48a9f774b57b20426
Arunjoseph3007 <arunjoseph3007@gmail.com>
2023-06-30 11:00:42 +0530

    Session list using id instead of username

--- src/components/PatientBox.js
+++ src/components/PatientBox.js
@@ -1,38 +1,33 @@
-import {
-    Flex,
-    Box, 
-    Heading,
-    Link,
-} from '@chakra-ui/react'
-import { ChevronRightIcon } from '@chakra-ui/icons'
+import { Flex, Box, Heading, Link } from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
 export default function PatientBox(props) {
-    return (
-        <div>
-            <Flex
-                mx="auto"
-                maxW="5xl"
-                my={4}
-                p={3}
-                rounded="md"
-                h="24"
-                alignItems={"center"}
-                shadow="md"
-                borderWidth={1}
-                borderColor="gray.100"
-            >
-                <Box flex={1}>
-                    <Heading fontWeight={"medium"} size="md">
-                        Name: {props.name}
-                    </Heading>
-                    <Heading fontWeight={"medium"} size="md">
-                        Age:{props.age}
-                    </Heading>
-                </Box>
-                <Link href={`/doctor/patient/${props.username}`}>
-                    <ChevronRightIcon fontSize={"4xl"} color={"GrayText"} />
-                </Link>
-            </Flex>
-        </div>
-    )
+  return (
+    <div>
+      <Flex
+        mx="auto"
+        maxW="5xl"
+        my={4}
+        p={3}
+        rounded="md"
+        h="24"
+        alignItems={"center"}
+        shadow="md"
+        borderWidth={1}
+        borderColor="gray.100"
+      >
+        <Box flex={1}>
+          <Heading fontWeight={"medium"} size="md">
+            Name: {props.name}
+          </Heading>
+          <Heading fontWeight={"medium"} size="md">
+            Age:{props.age}
+          </Heading>
+        </Box>
+        <Link href={`/doctor/patient/${props.id}`}>
+          <ChevronRightIcon fontSize={"4xl"} color={"GrayText"} />
+        </Link>
+      </Flex>
+    </div>
+  );
 }
